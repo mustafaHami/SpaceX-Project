327115f6146c0cf5c9e15bba38ebcf5d5ac71ea6
YPariset <yoann.pariset@edu.itescia.fr>
2022-11-24 10:45:00 +0100

    implementation boostrap and init project

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import Button from "./components/Button";
+
+const App = () => {
+    return (
+        <div class="card">
+            <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+                <Button></Button>
+            </div>
+        </div>
+          );
+}
+ 
+
+export default App;
---
+++ src/components/Button.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Button.css";
+
+const Button = () => {
+  return (
+    <button class="btn btn-primary">
+      Test
+    </button>
+  );
+};
+
+export default Button;
